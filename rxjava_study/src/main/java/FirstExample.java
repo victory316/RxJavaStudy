d8e69a44af94b5f509ba402f5fa933352c8c4983
Choi Seung Hwan <csh153@gmail.com>
2019-08-15 23:37:51 +0900

    before checkout

--- rxjava_study/src/main/java/FirstExample.java
+++ rxjava_study/src/main/java/FirstExample.java
@@ -1,4 +1,5 @@
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
 
 public class FirstExample {
     public void emit() {
@@ -7,7 +8,21 @@ public class FirstExample {
     }
 
     public static void main(String[] args) {
-        FirstExample demo = new FirstExample();
-        demo.emit();
+
+        Observable<Integer> source = Observable.create(
+                (ObservableEmitter<Integer> emitter) -> {
+                    emitter.onNext(100);
+                    emitter.onNext(200);
+                    emitter.onNext(300);
+                    emitter.onComplete();
+                }
+        );
+
+        Integer[] arr = {100, 200, 300};
+        Observable<Integer> source_2 = Observable.fromArray(arr);
+
+        source.subscribe(System.out::println);
+        source.subscribe(data -> System.out.println("Result : " + data));
+        source_2.subscribe(System.out::println);
     }
 }
