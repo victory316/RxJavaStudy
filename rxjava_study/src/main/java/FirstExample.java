3c52e19ca5bc9cd5ca5f450a9955bc872bd4fef9
Choi Seung Hwan <csh153@gmail.com>
2019-08-15 19:47:38 +0900

    first push

---
+++ rxjava_study/src/main/java/FirstExample.java
@@ -0,0 +1,13 @@
+import io.reactivex.Observable;
+
+public class FirstExample {
+    public void emit() {
+        Observable.just("Hello", "RxJava 2!!")
+                .subscribe(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        FirstExample demo = new FirstExample();
+        demo.emit();
+    }
+}
