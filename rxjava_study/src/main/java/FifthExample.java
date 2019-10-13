51f2e81e6ff6c58e9e7a7c920c104c96c405976f
Seunghwan Choi <seunghwanchoi@Seunghwanui-MacBookPro.local>
2019-10-13 14:10:31 +0900

    adding new class

---
+++ rxjava_study/src/main/java/FifthExample.java
@@ -0,0 +1,24 @@
+import common.CommonUtils;
+import common.Log;
+import common.Shape;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+public class FifthExample {
+
+    public static void main(String[] args) {
+
+        Observable.just("Hello", "RxJava 2!!")
+                .subscribe(Log::i);
+
+        String[] objs = {"1-S", "2-T", "3-P"};
+        Observable<String> source = Observable.fromArray(objs)
+                .doOnNext(data -> Log.v("Original data = " + data))
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .map(Shape::flip);
+
+        source.subscribe(Log::i);
+        CommonUtils.sleep(500);
+    }
+}
